c5a973c3d56653ef56e95e8883cf9e5bbe5fca95
sparshsaxenain <sparshsaxena3649@gmail.com>
2022-09-04 23:56:21 +0530

    Added all admin features and open browser automatically

--- src/main/java/com/prodapt/projectAlpha/ProjectAlphaApplication.java
+++ src/main/java/com/prodapt/projectAlpha/ProjectAlphaApplication.java
@@ -1,5 +1,7 @@
 package com.prodapt.projectAlpha;
 
+import java.io.IOException;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +10,17 @@ public class ProjectAlphaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProjectAlphaApplication.class, args);
+		try {
+			openHomePage();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
+	
+	private static void openHomePage() throws IOException {
+	       Runtime rt = Runtime.getRuntime();
+	       rt.exec("rundll32 url.dll,FileProtocolHandler " + "http://localhost:8080/loginform");
+	    }
 
 }
